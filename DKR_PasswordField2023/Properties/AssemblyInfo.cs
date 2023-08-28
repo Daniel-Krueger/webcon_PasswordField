7352608592ea34717b3413f0d9a8e8c127dec218
Daniel Krüger <daniel.krueger@cosmoconsult.com>
2023-08-28 20:27:53 +0200

    Initial version for BPS 2023

---
+++ DKR_PasswordField2023/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DKR_PasswordField2023")]
+[assembly: AssemblyDescription("WEBCON BPS Extensions")]
+[assembly: AssemblyProduct("DKR_PasswordField2023")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90f7cbb2-ce20-48f5-8c94-0c276d390ed2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.74")]
+[assembly: AssemblyFileVersion("1.0.0.74")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
